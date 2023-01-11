9dbd29c6423234404f12b4f5a90e29b94e444c2c
orpd0523 <orpd0523@gmail.com>
2023-01-10 19:41:12 -0600

    made keys make sound

--- script.js
+++ script.js
@@ -1,55 +1,66 @@
+const audioContext = new AudioContext();
+
 const NOTE_DETAILS = [
-    { note: "C", key: "Z", frequency: 261.626 },
-    { note: "Db", key: "S", frequency: 277.183 },
-    { note: "D", key: "X", frequency: 293.665 },
-    { note: "Eb", key: "D", frequency: 311.127 },
-    { note: "E", key: "C", frequency: 329.628 },
-    { note: "F", key: "V", frequency: 349.228 },
-    { note: "Gb", key: "G", frequency: 369.994 },
-    { note: "G", key: "B", frequency: 391.995 },
-    { note: "Ab", key: "H", frequency: 415.305 },
-    { note: "A", key: "N", frequency: 440 },
-    { note: "Bb", key: "J", frequency: 466.164 },
-    { note: "B", key: "M", frequency: 493.883 }
-  ]
+  { note: "C", key: "Z", frequency: 261.626 },
+  { note: "Db", key: "S", frequency: 277.183 },
+  { note: "D", key: "X", frequency: 293.665 },
+  { note: "Eb", key: "D", frequency: 311.127 },
+  { note: "E", key: "C", frequency: 329.628 },
+  { note: "F", key: "V", frequency: 349.228 },
+  { note: "Gb", key: "G", frequency: 369.994 },
+  { note: "G", key: "B", frequency: 391.995 },
+  { note: "Ab", key: "H", frequency: 415.305 },
+  { note: "A", key: "N", frequency: 440 },
+  { note: "Bb", key: "J", frequency: 466.164 },
+  { note: "B", key: "M", frequency: 493.883 },
+];
 
-  document.addEventListener("keydown", e => {
-    if (e.repeat) return 
-    const keyboardKey = e.code
-    const noteDetail = getNoteDetail(keyboardKey)
+document.addEventListener("keydown", (e) => {
+  if (e.repeat) return;
+  const keyboardKey = e.code;
+  const noteDetail = getNoteDetail(keyboardKey);
 
-    if (noteDetail == null) return
+  if (noteDetail == null) return;
 
-    noteDetail.active = true
-    playNotes()
-  })
+  noteDetail.active = true;
+  playNotes();
+});
 
-  document.addEventListener("keyup", e =>{
-    const keyboardKey = e.code
-    const noteDetail = getNoteDetail(keyboardKey)
+document.addEventListener("keyup", (e) => {
+  const keyboardKey = e.code;
+  const noteDetail = getNoteDetail(keyboardKey);
 
-    if (noteDetail == null) return
+  if (noteDetail == null) return;
 
-    noteDetail.active = false
-    playNotes()
-  })
+  noteDetail.active = false;
+  playNotes();
+});
 
-  function getNoteDetail(keyboardKey){
-    return NOTE_DETAILS.find(n => `Key${n.key}` === keyboardKey)
-  }
+function getNoteDetail(keyboardKey) {
+  return NOTE_DETAILS.find((n) => `Key${n.key}` === keyboardKey);
+}
 
-  function playNotes() {
-    NOTE_DETAILS.forEach(n =>{
-      const keyElement = document.querySelector(`[data-note="${n.note}"]`)
-      keyElement.classList.toggle("active", n.active || false)
-    })
-
-    const activeNotes = NOTE_DETAILS.filter(n => n.active)
-    activeNotes.forEach(n =>{
-      startNote(n)
-    })
-
-    function startNote(noteDetail){
-
+function playNotes() {
+  NOTE_DETAILS.forEach((n) => {
+    const keyElement = document.querySelector(`[data-note="${n.note}"]`);
+    keyElement.classList.toggle("active", n.active || false);
+    if (n.oscillator != null){
+      n.oscillator.stop()
+      n.oscillator.disconnect()
     }
-  }
+  });
+
+  const activeNotes = NOTE_DETAILS.filter((n) => n.active);
+  activeNotes.forEach((n) => {
+    startNote(n);
+  });
+}
+
+function startNote(noteDetail) {
+  const oscillator = audioContext.createOscillator()
+  oscillator.frequency = noteDetail.frequency
+  oscillator.type = "sine"
+  oscillator.connect(audioContext.destination)
+  oscillator.start()
+  noteDetail.oscillator = oscillator
+}
